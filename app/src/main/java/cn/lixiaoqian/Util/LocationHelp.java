7a39b84415e57584aa72cdb3c370463a271ff439
施纯评 <306806106@qq.com>
2020-12-25 13:40:10 +0800

    #高德定位

--- app/src/main/java/cn/lixiaoqian/Util/LocationHelp.java
+++ app/src/main/java/cn/lixiaoqian/Util/LocationHelp.java
@@ -26,11 +26,22 @@ import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationClientOption.AMapLocationMode;
+import com.amap.api.location.AMapLocationClientOption.AMapLocationProtocol;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.location.AMapLocationQualityReport;
+
 public class LocationHelp extends Activity {
+
+    private AMapLocationClient locationClient = null;
+    private AMapLocationClientOption locationOption = null;
 
     private String locationProvider;
     private LocationManager locationManager;
-    private MyLocationListener myLocationListener;
+//    private MyLocationListener myLocationListener;
     private AudioHelper audioHelper;
     private Context context;
     private int targetSdkVersion;
@@ -56,6 +67,7 @@ public class LocationHelp extends Activity {
         this.locationManager = locationManager;
         this.locationProvider = locationProvider;
         this.audioHelper =  audioHelper;
+        InitLocation();
         this.audioHelper.setListener(new AudioHelper.onListener() {
             @Override
             public void OnStartListener() {
@@ -75,6 +87,158 @@ public class LocationHelp extends Activity {
 
             }
         });
+    }
+
+
+
+    //region 定位
+
+    /**
+     * 初始化定位
+     */
+    private void InitLocation(){
+//        Log.d(TAG, "InitLocation: ");
+        //初始化client
+        locationClient = new AMapLocationClient(this.context);
+        locationOption = getDefaultOption();
+        //设置定位参数
+        locationClient.setLocationOption(locationOption);
+        // 设置定位监听
+        locationClient.setLocationListener(locationListener);
+    }
+
+    private AMapLocationClientOption getDefaultOption(){
+        AMapLocationClientOption mOption = new AMapLocationClientOption();
+        mOption.setLocationMode(AMapLocationMode.Hight_Accuracy);//可选，设置定位模式，可选的模式有高精度、仅设备、仅网络。默认为高精度模式
+        mOption.setGpsFirst(false);//可选，设置是否gps优先，只在高精度模式下有效。默认关闭
+        mOption.setHttpTimeOut(30000);//可选，设置网络请求超时时间。默认为30秒。在仅设备模式下无效
+        mOption.setInterval(2000);//可选，设置定位间隔。默认为2秒
+        mOption.setNeedAddress(true);//可选，设置是否返回逆地理地址信息。默认是true
+        mOption.setOnceLocation(false);//可选，设置是否单次定位。默认是false
+        mOption.setOnceLocationLatest(false);//可选，设置是否等待wifi刷新，默认为false.如果设置为true,会自动变为单次定位，持续定位时不要使用
+        AMapLocationClientOption.setLocationProtocol(AMapLocationProtocol.HTTP);//可选， 设置网络请求的协议。可选HTTP或者HTTPS。默认为HTTP
+        mOption.setSensorEnable(false);//可选，设置是否使用传感器。默认是false
+        mOption.setWifiScan(true); //可选，设置是否开启wifi扫描。默认为true，如果设置为false会同时停止主动刷新，停止以后完全依赖于系统刷新，定位位置可能存在误差
+        mOption.setLocationCacheEnable(true); //可选，设置是否使用缓存定位，默认为true
+        mOption.setGeoLanguage(AMapLocationClientOption.GeoLanguage.DEFAULT);//可选，设置逆地理信息的语言，默认值为默认语言（根据所在地区选择语言）
+        return mOption;
+    }
+
+    /**
+     * 定位监听
+     */
+    AMapLocationListener locationListener = new AMapLocationListener() {
+        private String latLongString;
+        @Override
+        public void onLocationChanged(AMapLocation location) {
+            if (null != location) {
+                if(location.getErrorCode() == 0) {
+                    double longitude = location.getLongitude();
+                    double latitude = location.getLatitude();
+//                  Log.d(TAG, "locationListener longitude: "+longitude);
+//                  Log.d(TAG, "locationListener latitude: "+latitude);
+                    LatLonPoint latLonPoint = GpsCorrect.ToGPSPoint(latitude, longitude);
+                    latLongString = latLonPoint.getLatitude() + "|" + latLonPoint.getLongitude();
+//                  Log.d(TAG, "locationService getgps: " + latLongString);
+                    //latLongString = GpsCorrect.GetGpsCorrect(latitude,longitude);
+                    //Log.d(TAG, "locationService getgpscorrect: " + latLongString);
+                    UnityPlayer.UnitySendMessage("LocationManager", "LocResult", latLongString);
+                }
+            }
+//                StringBuffer sb = new StringBuffer();
+//                //errCode等于0代表定位成功，其他的为定位失败，具体的可以参照官网定位错误码说明
+//                if(location.getErrorCode() == 0){
+//                    sb.append("定位成功" + "\n");
+//                    sb.append("定位类型: " + location.getLocationType() + "\n");
+//                    sb.append("经    度    : " + location.getLongitude() + "\n");
+//                    sb.append("纬    度    : " + location.getLatitude() + "\n");
+//                    sb.append("精    度    : " + location.getAccuracy() + "米" + "\n");
+//                    sb.append("提供者    : " + location.getProvider() + "\n");
+//
+//                    sb.append("速    度    : " + location.getSpeed() + "米/秒" + "\n");
+//                    sb.append("角    度    : " + location.getBearing() + "\n");
+//                    // 获取当前提供定位服务的卫星个数
+//                    sb.append("星    数    : " + location.getSatellites() + "\n");
+//                    sb.append("国    家    : " + location.getCountry() + "\n");
+//                    sb.append("省            : " + location.getProvince() + "\n");
+//                    sb.append("市            : " + location.getCity() + "\n");
+//                    sb.append("城市编码 : " + location.getCityCode() + "\n");
+//                    sb.append("区            : " + location.getDistrict() + "\n");
+//                    sb.append("区域 码   : " + location.getAdCode() + "\n");
+//                    sb.append("地    址    : " + location.getAddress() + "\n");
+//                    sb.append("兴趣点    : " + location.getPoiName() + "\n");
+//                } else {
+//                    //定位失败
+//                    sb.append("定位失败" + "\n");
+//                    sb.append("错误码:" + location.getErrorCode() + "\n");
+//                    sb.append("错误信息:" + location.getErrorInfo() + "\n");
+//                    sb.append("错误描述:" + location.getLocationDetail() + "\n");
+//                }
+//                sb.append("***定位质量报告***").append("\n");
+//                sb.append("* WIFI开关：").append(location.getLocationQualityReport().isWifiAble() ? "开启":"关闭").append("\n");
+//                sb.append("* GPS状态：").append(getGPSStatusString(location.getLocationQualityReport().getGPSStatus())).append("\n");
+//                sb.append("* GPS星数：").append(location.getLocationQualityReport().getGPSSatellites()).append("\n");
+//                sb.append("* 网络类型：" + location.getLocationQualityReport().getNetworkType()).append("\n");
+//                sb.append("* 网络耗时：" + location.getLocationQualityReport().getNetUseTime()).append("\n");
+//                sb.append("****************").append("\n");
+//            } else {
+////                tvResult.setText("定位失败，loc is null");
+//            }
+        }
+    };
+
+
+    /**
+     * 获取GPS状态的字符串
+     * @param statusCode GPS状态码
+     * @return
+     */
+    private String getGPSStatusString(int statusCode){
+        String str = "";
+        switch (statusCode){
+            case AMapLocationQualityReport.GPS_STATUS_OK:
+                str = "GPS状态正常";
+                break;
+            case AMapLocationQualityReport.GPS_STATUS_NOGPSPROVIDER:
+                str = "手机中没有GPS Provider，无法进行GPS定位";
+                break;
+            case AMapLocationQualityReport.GPS_STATUS_OFF:
+                str = "GPS关闭，建议开启GPS，提高定位质量";
+                break;
+            case AMapLocationQualityReport.GPS_STATUS_MODE_SAVING:
+                str = "选择的定位模式中不包含GPS定位，建议选择包含GPS定位的模式，提高定位质量";
+                break;
+            case AMapLocationQualityReport.GPS_STATUS_NOGPSPERMISSION:
+                str = "没有GPS定位权限，建议开启gps定位权限";
+                break;
+        }
+        Log.d(TAG, "getGPSStatusString: "+str);
+        return str;
+    }
+
+    private void StartLocation(){
+//        Log.d(TAG, "StartLocation: ");
+        // 设置定位参数
+        locationClient.setLocationOption(locationOption);
+        // 启动定位
+        locationClient.startLocation();
+    }
+
+    private void StopLocation(){
+        // 停止定位
+        locationClient.stopLocation();
+    }
+
+    private void DestroyLocation(){
+        if (null != locationClient) {
+            /**
+             * 如果AMapLocationClient是在当前Activity实例化的，
+             * 在Activity的onDestroy中一定要执行AMapLocationClient的onDestroy
+             */
+            locationClient.onDestroy();
+            locationClient = null;
+            locationOption = null;
+        }
     }
 
     public void OnLocation(Activity activity) {
@@ -98,54 +262,56 @@ public class LocationHelp extends Activity {
         }
         //Log.d(TAG, "locationManager：" + ret);
         if (ret) {
-            myLocationListener = new MyLocationListener();
-            locationManager.requestLocationUpdates(locationProvider, 0, 0, myLocationListener);
+              StartLocation();
+//            myLocationListener = new MyLocationListener();
+//            locationManager.requestLocationUpdates(locationProvider, 0, 0, myLocationListener);
         } else {
             ActivityCompat.requestPermissions(activity, PERMISSION_LOCATION, GET_LOCATION);
         }
     }
 
-    private class MyLocationListener implements LocationListener {
-        private String latLongString;
+//    private class MyLocationListener implements LocationListener {
+//        private String latLongString;
+//
+//        //当定位位置改变的调用的方法
+//        //Location : 当前的位置
+//        @Override
+//        public void onLocationChanged(Location location) {
+//            float accuracy = location.getAccuracy();//获取精确位置
+//            double altitude = location.getAltitude();//获取海拔
+//            final double latitude = location.getLatitude();//获取纬度，平行
+//            final double longitude = location.getLongitude();//获取经度，垂直
+//            latLongString = latitude + "|" + longitude;
+//            Log.d(TAG, "locationService getgps: " + latLongString);
+//            //latLongString = GpsCorrect.GetGpsCorrect(latitude,longitude);
+//            Log.d(TAG, "locationService getgpscorrect: " + latLongString);
+//            UnityPlayer.UnitySendMessage("LocationManager", "LocResult", latLongString);
+//            SetPlay(longitude, latitude);
+//        }
+//
+//        //当定位状态发生改变的时候调用的方式
+//        @Override
+//        public void onStatusChanged(String provider, int status, Bundle extras) {
+//            // TODO Auto-generated method stub
+//            Log.d(TAG, "myservice status：" + status);
+//        }
+//
+//        //当定位可用的时候调用的方法
+//        @Override
+//        public void onProviderEnabled(String provider) {
+//            // TODO Auto-generated method stub
+//            Log.d(TAG, "myservice onProviderEnabled");
+//        }
+//
+//        //当定位不可用的时候调用的方法
+//        @Override
+//        public void onProviderDisabled(String provider) {
+//            // TODO Auto-generated method stub
+//            Log.d(TAG, "myservice onProviderDisabled");
+//        }
+//    }
 
-        //当定位位置改变的调用的方法
-        //Location : 当前的位置
-        @Override
-        public void onLocationChanged(Location location) {
-            float accuracy = location.getAccuracy();//获取精确位置
-            double altitude = location.getAltitude();//获取海拔
-            final double latitude = location.getLatitude();//获取纬度，平行
-            final double longitude = location.getLongitude();//获取经度，垂直
-            latLongString = latitude + "|" + longitude;
-            Log.d(TAG, "locationService getgps: " + latLongString);
-            //latLongString = GpsCorrect.GetGpsCorrect(latitude,longitude);
-            Log.d(TAG, "locationService getgpscorrect: " + latLongString);
-            UnityPlayer.UnitySendMessage("LocationManager", "LocResult", latLongString);
-            SetPlay(longitude, latitude);
-        }
-
-        //当定位状态发生改变的时候调用的方式
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-            // TODO Auto-generated method stub
-            Log.d(TAG, "myservice status：" + status);
-        }
-
-        //当定位可用的时候调用的方法
-        @Override
-        public void onProviderEnabled(String provider) {
-            // TODO Auto-generated method stub
-            Log.d(TAG, "myservice onProviderEnabled");
-        }
-
-        //当定位不可用的时候调用的方法
-        @Override
-        public void onProviderDisabled(String provider) {
-            // TODO Auto-generated method stub
-            Log.d(TAG, "myservice onProviderDisabled");
-        }
-    }
-
+    //endregion
     public void OnMinDistance(float minDistance)
     {
         this.minArriveDistance = minDistance;
